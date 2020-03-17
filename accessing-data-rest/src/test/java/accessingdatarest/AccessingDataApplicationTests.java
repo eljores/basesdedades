a0eba11c5cb067607dbac701eaebb1561912c8a4
eljores <59867840+eljores@users.noreply.github.com>
2020-03-17 17:19:31 +0100

    Add files via upload

---
+++ accessing-data-rest/src/test/java/accessingdatarest/AccessingDataApplicationTests.java
@@ -0,0 +1,13 @@
+package accessingdatarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
